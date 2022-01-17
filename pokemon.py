5277149a757b22302855594b1e3cc7d36923b0f1
Veli Akiner <veli.akiner@gmail.com>
2022-01-17 00:22:11 +0000

    pokemon data and scripts

--- pokemon.py
+++ pokemon.py
@@ -3,21 +3,18 @@ import csv
 from collections import defaultdict
 with open('Pokemon.csv', newline='') as csvfile:
      reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-     indexed_by_stats = defaultdict(list)
+     json_dump = {}
+     next(reader)
      for row in reader:
           number, name, type1, type2, total, hp, attack, defense, sp_attack, sp_defense, speed, generation, legendary = row
-          formes_to_ignore = "Gigantamax",
+          formes_to_ignore = "Gigantamax", "Mega"
           if (any(f in name.split(" ") for f in formes_to_ignore)):
                continue
           type1, type2 = sorted((type1, type2))
-          indexed_by_stats[(hp, attack, defense, sp_attack, sp_defense, speed, type1, type2)].append(name)
-     # import pdb;pdb.set_trace()
-     unique = 0
-     for k, v in indexed_by_stats.items():
-          if len(v) > 1:
-               print(k, v)
-          else:
-               unique += 1
-     with open("pokemon.json", "w") as f:
-          f.write(json.dumps(indexed_by_stats))
+          json_dump[name] = [generation, type1, type2, hp, attack, defense, sp_attack, sp_defense, speed]
+          if len(json_dump.keys()) > 10:
+               break
+     with open("src/pokemon.json", "w") as f:
+          f.write(json.dumps(json_dump))
+
 
