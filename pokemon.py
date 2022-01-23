8412e8aa03834ab224855f21c126ff63e7473e11
Veli Akiner <veli.akiner@gmail.com>
2022-01-23 14:13:43 +0000

    put every pokemon into generation buckets

--- pokemon.py
+++ pokemon.py
@@ -1,22 +1,20 @@
 import json
 import csv
+
 from collections import defaultdict
 with open('Pokemon.csv', newline='') as csvfile:
-     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-     json_dump = {}
-     next(reader)
-     for row in reader:
-          number, name, type1, type2, total, hp, attack, defense, sp_attack, sp_defense, speed, generation, legendary = row
-          formes_to_ignore = "Gigantamax", "Mega"
-          if (any(f in name.split(" ") for f in formes_to_ignore)):
-               continue
-          type1, type2 = sorted((type1, type2))
-          if int(generation) > 3:
-               continue
-          json_dump[name] = [generation, type1, type2, hp, attack, defense, sp_attack, sp_defense, speed]
-          # if len(json_dump.keys()) > 10:
-          #      break
-     with open("src/pokemon.json", "w") as f:
-          f.write(json.dumps(json_dump))
+    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    all_gens = defaultdict(dict)
+    next(reader)
+    for row in reader:
+        number, name, type1, type2, total, hp, attack, defense, sp_attack, sp_defense, speed, generation, legendary = row
+        formes_to_ignore = "Gigantamax", "Mega"
+        if (any(f in name.split(" ") for f in formes_to_ignore)):
+            continue
+        type1, type2 = sorted((type1, type2))
+        all_gens[generation][name] = [generation, type1, type2, hp, attack, defense, sp_attack, sp_defense, speed]
+    with open("src/allGens.json", "w") as f:
+        f.write(json.dumps(all_gens,indent=4, sort_keys=True))
+
 
 
