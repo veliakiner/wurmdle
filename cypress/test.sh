7111b9fc440793160a56fc01330a4213e14ffc51
Veli Akiner <veli.akiner@gmail.com>
2022-02-06 11:52:42 +0000

    Tweaks test.sh

--- cypress/test.sh
+++ cypress/test.sh
@@ -1,4 +1,15 @@
-trap "kill 0" EXIT
+# Helper script for running Cypress tests against app without having to start the app separately
 
-BROWSER=none react-scripts start &
+BROWSER=none react-scripts start&
+APP_PID=$!
+
+
+cleanup () {
+    pkill -P $APP_PID
+    exit $EXIT_CODE
+}
+
+trap cleanup EXIT
+
 npx cypress run
+EXIT_CODE=$?
