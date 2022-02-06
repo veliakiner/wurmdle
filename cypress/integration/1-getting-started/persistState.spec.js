64f33b0fe8af998faeb8cb26d07e8dfb3aff6da6
Veli Akiner <veli.akiner@gmail.com>
2022-02-06 15:03:40 +0000

    refactor tests

---
+++ cypress/integration/1-getting-started/persistState.spec.js
@@ -0,0 +1,16 @@
+import GamePage from "../../pageObjects/gamePage";
+
+describe("Player can resume their game when they navigate away", () => {
+  it("opens the app", () => {
+    cy.visit("http://localhost:3000/");
+  });
+  it("Guesses incorrectly", () => {
+    GamePage.input().type("Mew").type("{enter}");
+  });
+  it("opens the app again", () => {
+    cy.reload();
+  });
+  it("There should be two rows (1 label row, 1 guess rows", () => {
+    GamePage.row().should('have.length', 2);
+  });
+});
