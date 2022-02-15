eee1b4afdaf9448a455c670e05e32b70fcf6f20d
Veli Akiner <veli.akiner@gmail.com>
2022-02-15 21:33:19 +0000

    add test case for submit button

--- cypress/integration/1-getting-started/todo.spec.js
+++ cypress/integration/1-getting-started/todo.spec.js
@@ -68,13 +68,6 @@ describe("Invalid guess", () => {
   it("The input should glow", () => {
     GamePage.incorrectInput().should('be.visible')
   });
-  // it("The input box should clear", () => {
-  //   GamePage.input().should('have.value', "")
-  // });
-  // it("Makes an invalid guess", () => {
-  //   GamePage.input().type("Pikachu").type("{enter}");
-  //   GamePage.row().should('have.length', 2)
-  // });
 });
 
 describe("Player wins the game on the last guess", () => {
@@ -98,5 +91,27 @@ describe("Player wins the game on the last guess", () => {
   });
 });
 
-
-
+describe("Using the submit button", () => {
+  it("opens the app", () => {
+    cy.visit("http://localhost:3000/Pikachu");
+  });
+  it("Makes an invalid guess", () => {
+    GamePage.input().type("Pikablu");
+  });
+  it("Hit the button", () => {
+    GamePage.submitButton().click();
+  });
+  it("The input should glow", () => {
+    GamePage.incorrectInput().should('be.visible')
+  });
+  it("The input box should clear", () => {
+    GamePage.input().should('have.value', "")
+  });
+  it("Makes a valid guess", () => {
+    GamePage.input().type("Mew");
+    GamePage.submitButton().click();
+  });
+  it("There should be two rows (1 label row, and 1 guess row", () => {
+    GamePage.row().should('have.length', 2)
+  });
+});
