80d99b87dc9b04f75a20e7b3ace630a982e6aa4a
Veli Akiner <veli.akiner@gmail.com>
2022-02-17 13:03:16 +0000

    move individual test steps into same test

--- cypress/integration/1-getting-started/persistState.spec.js
+++ cypress/integration/1-getting-started/persistState.spec.js
@@ -1,17 +1,10 @@
 import GamePage from "../../pageObjects/gamePage";
 
-// Currently fails because Cypress trashes local storage when you refresh a page 
-// describe("Player can resume their game when they navigate away", () => {
-//   it("opens the app", () => {
-//     cy.visit("http://localhost:3000/");
-//   });
-//   it("Guesses incorrectly", () => {
-//     GamePage.input().type("Mew").type("{enter}");
-//   });
-//   it("opens the app again", () => {
-//     cy.reload();
-//   });
-//   it("There should be two rows (1 label row, 1 guess rows", () => {
-//     GamePage.row().should('have.length', 2);
-//   });
-// });
+describe("Player can resume their game when they navigate away", () => {
+  it("opens the app and reloads halfway through the test", () => {
+    cy.visit("http://localhost:3000/");
+    GamePage.input().type("Mew").type("{enter}");
+    cy.reload();
+    GamePage.row().should('have.length', 2);
+  });
+});
--- cypress/integration/1-getting-started/slider.spec.js
+++ cypress/integration/1-getting-started/slider.spec.js
@@ -12,56 +12,22 @@ afterEach(function () {
 describe("The slider lets you select different mons", () => {
   it("opens the app", () => {
     cy.visit("http://localhost:3000/Pikachu");
-  });
-  it("Open the settings page", () => {
     GamePage.settingsToggle().click();
-  });
-  it("Select only Gen 1", () => {
     SettingsPage.rightSlider().type("{leftArrow}").type("{leftArrow}")
-  });
-  it("Go back to the game page", () => {
     SettingsPage.settingsToggle().click();
-  });
-  it("Try to select a gen 2 mon", () => {
     GamePage.input().type("Smeargle").type("{enter}");
-  });
-  it("There should be 1 row (1 label row, 0  guess rows", () => {
     GamePage.row().should('have.length', 1)
-  });
-  it("The input should glow", () => {
     GamePage.incorrectInput().should('be.visible')
-  });
-  it("Open the settings page", () => {
     GamePage.settingsToggle().click();
-  });
-  it("Select only Gen 4", () => {
     SettingsPage.leftSlider().type("{rightArrow}").type("{rightArrow}").type("{rightArrow}");
-  });
-  it("Go back to the game page", () => {
     SettingsPage.settingsToggle().click();
-  });
-  it("Try to select a gen 4 mon", () => {
     GamePage.input().type("Heatran").type("{enter}");
-  });
-  it("There should be 2 rows (1 label row, 1  guess rows", () => {
     GamePage.row().should('have.length', 2)
-  });
-  it("Open the settings page", () => {
     GamePage.settingsToggle().click();
-  });
-  it("Try to move the slider to include gen 1", () => {
     SettingsPage.leftSlider().type("{leftArrow}").type("{leftArrow}").type("{leftArrow}");
-  });
-  it("Go back to the game page", () => {
     SettingsPage.settingsToggle().click();
-  });
-  it("Try to select a gen 1 mon", () => {
     GamePage.input().type("Mew").type("{enter}");
-  });
-  it("The input should glow", () => {
     GamePage.incorrectInput().should('be.visible')
-  });
-  it("There should be 2 rows (1 label row, 1  guess rows", () => {
     GamePage.row().should('have.length', 2)
   });
 });
