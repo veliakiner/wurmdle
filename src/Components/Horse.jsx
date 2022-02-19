1ee8eeb318f80c30760787bbac2a07751a3d03fe
Veli Akiner <veli.akiner@gmail.com>
2022-02-19 01:07:23 +0000

    Redirect (#39)
    * redirect
    
    * tweak

--- src/Components/Horse.jsx
+++ src/Components/Horse.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Horse() {
-  return <img alt="" style={{ margin: 'auto', width: '100%', animation: 'spin 3s linear infinite' }} src="https://media.discordapp.net/attachments/851532855167418419/944370081339674665/horse-in-an-upstairs-window-anna-villarreal-garbis.png" />;
+  return <img alt="" style={{ animation: 'spin 3s linear infinite' }} src="https://media.discordapp.net/attachments/851532855167418419/944370081339674665/horse-in-an-upstairs-window-anna-villarreal-garbis.png" />;
 }
