96adeb6b1faea36a39409a0255806b36a5e08476
Veli Akiner <veli.akiner@gmail.com>
2022-02-19 00:31:53 +0000

    share option lol

---
+++ src/Components/Horse.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Horse() {
+  return <img alt="" style={{ margin: 'auto', width: '100%', animation: 'spin 3s linear infinite' }} src="https://media.discordapp.net/attachments/851532855167418419/944370081339674665/horse-in-an-upstairs-window-anna-villarreal-garbis.png" />;
+}
