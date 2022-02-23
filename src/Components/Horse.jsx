2086feb305e1f4e17c32b6f45d2568fc7c13d58d
Veli Akiner <veli.akiner@gmail.com>
2022-02-23 11:30:57 +0000

    remove share route

--- src/Components/Horse.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Horse() {
-  return <img alt="" style={{ animation: 'spin 3s linear infinite' }} src="https://media.discordapp.net/attachments/851532855167418419/944370081339674665/horse-in-an-upstairs-window-anna-villarreal-garbis.png" />;
-}
