e0744b65ab0459b72fa4e44dac348acafa380c92
Veli Akiner <veli.akiner@gmail.com>
2022-07-03 19:30:49 +0100

    add more unit tests

--- src/Libraries/Pokemon/utils.test.jsx
+++ src/Libraries/Pokemon/utils.test.jsx
@@ -8,3 +8,9 @@ test('returns 151 for the OG generation', () => {
 test('returns 81 for the fully-evolved OG generation', () => {
   expect(getMonsList([1, 1], true)).toHaveLength(81);
 });
+test('Onix should be fully evolved in gen 1 only', () => {
+  expect(getMonsList([1, 1], true)).toContain("Onix");
+});
+test('Onix should not be fully evolved in gens 1 to 2', () => {
+  expect(getMonsList([1, 2], true)).not.toContain("Onix");
+});
